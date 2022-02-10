39fd6a64842f70ac22fd9454db5eecd4fb659fcc
gef <2455694087@qq.com>
2022-02-10 15:48:50 -0600

    conf

--- recon.py
+++ recon.py
@@ -9,7 +9,8 @@ from pyspark.conf import SparkConf
 from pyspark.sql import SparkSession
 from pyspark import AccumulatorParam
 from openpyxl import load_workbook
-
+from collections import namedtuple
+import helper
 CONF_PATH = "conf.yml"
 EXCEL_PATH = "test.xlsx"
 
@@ -67,6 +68,11 @@ class SparkDataReconJob(SparkDataFrameJob):
         df3 = df1.intersect(df2)
         df3.show()
 
+    def test_execute(self):
+        df1 = self.spark.createDataFrame(data=[(1, 2), (3, 4)], schema=['a', 'b'])
+        df2 = self.spark.createDataFrame(data=[(1, 2), (3, 4)], schema=['a', 'b'])
+        res = self.field_to_field_compare(df1,df2,'a')
+        self.post(res)
     def execute(self):
         conf = YMLConfBuilder().get_conf(CONF_PATH)
 
@@ -81,8 +87,8 @@ class SparkDataReconJob(SparkDataFrameJob):
         """
         (key1,[(row,1) (row,2)])
         """
-        def combine_key_to_group_rdd(x):
-            key = x[0]
+        def unite_same_field(x):
+            # key = x[0]
             rows = x[1]
             count_cols = len(rows[0][0])
             res = tuple()
@@ -101,11 +107,22 @@ class SparkDataReconJob(SparkDataFrameJob):
             return res
         def count_mismatch(x,acc):
             column_names = column_names_str.split(",")
+            # if key missing, which means the record not exist
+            # then skip the mismatch
+            # we count it as
+            if x[2 * key_index] is None or x[2 * key_index + 1] is None:
+                return 1
+
             for idx in range(len(column_names)):
-                if x[2*idx] is None or x[2*idx + 1] == None:
-                    if idx != key_index:
-                        acc.add(column_names[idx])
-                elif x[2*idx] != x[2*idx + 1]:
+                # if either of them don have primary key, continue, that means it's missing
+                # should we throw exception?
+                #
+                # if x[2*idx] is None or x[2*idx + 1] == None:
+                #     if idx != key_index:
+                #         acc.add(column_names[idx])
+                # elif x[2*idx] != x[2*idx + 1]:
+                #     acc.add(column_names[idx])
+                if x[2 * idx] != x[2 * idx + 1]:
                     acc.add(column_names[idx])
             # acc.add("qwe")
             return 1
@@ -121,28 +138,43 @@ class SparkDataReconJob(SparkDataFrameJob):
         print("missing from src2:")
         df_missing_from_src2.show()
 
-
+        # change (field1,...key,..fieldn) => (key, (field1,...)), so we can combine key
+        # union to dataset together so we can group by key
+        # after map: (key,iterable[RowWithSameKey])
         key_index = df1.columns.index(key)
         rdd1 = df1.rdd.map(lambda x : (x[key_index],(x, 1)))
         rdd2 = df2.rdd.map(lambda x : (x[key_index],(x, 2)))
         union_rdd = rdd1.union(rdd2)
-
         grouped_union_rdd = union_rdd.groupByKey().map(lambda x : (x[0], list(x[1])))
-        print(grouped_union_rdd.collect())
+        # print("example after group by key:")
+        # print(grouped_union_rdd.collect()[0])
+
         # this rdd is: field1_src1, field1_src2, ....fieldx_src1, fieldx_src2
-        aggr_rdd = grouped_union_rdd.map(lambda x : combine_key_to_group_rdd(x))
-        print(aggr_rdd.collect())
-        # defined a accumulator
+        # this is what we need in field to field compare sheet
+        aggr_rdd = grouped_union_rdd.map(lambda x : helper.unite_same_field(x)).cache()
+
+        # print("example after we put the same fields together:")
+        # print(aggr_rdd)
+        # aggr_df = self.spark.createDataFrame(data=aggr_rdd.collect(), schema=["_{}".format(str(i+1)) for i in range(2*len(df1.columns))]).toPandas()
+        # aggr_df = aggr_rdd.toDF(["_{}".format(str(i+1)) for i in range(2*len(df1.columns))]).toPandas()
+        # print(aggr_df)
+
+        # defined a accumulator to count mismatch
         acc = self.spark.sparkContext.accumulator('initial', DictAccumulatorParam())
         acc.value = json.dumps({}, sort_keys=True)
 
         column_names_str = ",".join(df1.columns)
 
+        # count mismatch by the field to field rdd
+        # filterRDD is less confusing, this is just an action to put data in accumulator
+        # we won't use the count_mismatch_rdd.count()
         count_mismatch_rdd = aggr_rdd.map(lambda x : count_mismatch(x,acc))
         count_mismatch_rdd.count()
-        # print(type(df1.columns))
-        # print(acc)
+
         res = json.loads(acc.value)
+        # this map is divided by partition, need to merge
+        # example: {'{"b": 1}': 1, '{"a":1}': 5} => {'a':5, 'b':1}
+        print(res)
         sum_up_count = {}
         for item in res.items():
             sub_res = json.loads(item[0])
@@ -152,15 +184,32 @@ class SparkDataReconJob(SparkDataFrameJob):
                     sum_up_count[sub_item[0]] = 0
                 sum_up_count[sub_item[0]] = sum_up_count[sub_item[0]] + sub_item[1] * item[1]
         print(sum_up_count)
+
         audit = []
-        for item in sum_up_count.items():
-            audit.append({"Column":item[0],
-                          "Mismatch":item[1],
-                          "Missing from src1":count_missing_sr1,
-                          "Missing from src2":count_missing_sr2})
+
+        for field in df1.columns:
+            if field in sum_up_count.keys():
+                audit.append({"Column": field,
+                              "Mismatch": sum_up_count[field],
+                              "Missing from src1": count_missing_sr1,
+                              "Missing from src2": count_missing_sr2})
+            else:
+                audit.append({"Column": field,
+                              "Mismatch": 0,
+                              "Missing from src1": count_missing_sr1,
+                              "Missing from src2": count_missing_sr2})
+
         df_audit = pd.DataFrame(data=audit)
+        # aggr_df = self.spark.createDataFrame(data=aggr_rdd.collect(),
+        #                                      schema=["_{}".format(str(i + 1)) for i in
+        #                                              range(2 * len(df1.columns))]).toPandas()
         # df_audit.to_excel(EXCEL_PATH, sheet_name='audit')
-        aggr_df = aggr_rdd.toDF().toPandas()
+        try:
+            aggr_df = aggr_rdd.toDF().toPandas()
+        except:
+            aggr_df = self.spark.createDataFrame(data=aggr_rdd.collect(),
+                                                 schema=["_{}".format(str(i + 1)) for i in range(2 * len(df1.columns))]).toPandas()
+            print(aggr_df)
         return (aggr_df,df1.columns, df_audit)
 
 
@@ -270,6 +319,65 @@ class DictAccumulatorParam(AccumulatorParam):
 
         return json.dumps(value1, sort_keys=True)
 
-SparkDataReconJob().execute()
+
+
+class Mock:
+
+    def generate(self):
+        mocked_model = namedtuple("mocked", ['a', 'b'])
+        src1 = [mocked_model(1,2), mocked_model(3,4)]
+        src2 = [mocked_model(1,3), mocked_model(5,6)]
+
+        # insert hive, two different table
+        sparkSession = (SparkSession
+                        .builder
+                        .appName('example-pyspark-read-and-write-from-hive')
+                        .config("hive.metastore.uris", "thrift://localhost:9083", conf=SparkConf())
+                        .enableHiveSupport()
+                        .getOrCreate()
+                        )
+
+        df1 = sparkSession.createDataFrame(src1)
+        df2 = sparkSession.createDataFrame(src2)
+
+        sparkSession.sql("DROP TABLE IF EXISTS test1")
+        df1.write.saveAsTable("test1")
+
+        sparkSession.sql("DROP TABLE IF EXISTS test2")
+        df2.write.saveAsTable("test2")
+    def verify(self):
+        sparkSession = (SparkSession
+                        .builder
+                        .appName('example-pyspark-read-and-write-from-hive')
+                        .config("hive.metastore.uris", "thrift://localhost:9083", conf=SparkConf())
+                        .enableHiveSupport()
+                        .getOrCreate()
+                        )
+
+        sparkSession.sql("select * from test1").show()
+        sparkSession.sql("select * from test2").show()
+class UnitTest:
+    def test_spark_recon(self):
+        sparkSession = (SparkSession
+                        .builder
+                        .appName('example-pyspark-read-and-write-from-hive')
+                        .config("hive.metastore.uris", "thrift://localhost:9083", conf=SparkConf())
+                        .enableHiveSupport()
+                        .getOrCreate()
+                        )
+
+        df1 = sparkSession.sql("select * from test1")
+        df2 = sparkSession.sql("select * from test2")
+
+        recon = SparkDataReconJob()
+        res = recon.field_to_field_compare(df1,df2,'a')
+        recon.post(res)
+# SparkDataReconJob().execute()
 # SparkDataReconExcelFormatter().format_field_to_field_excel(EXCEL_PATH)
 # SparkDataReconJob().audit(1,2)
+# SparkDataReconJob().test()
+# SparkDataReconJob().test_execute()
+# mock = Mock()
+# mock.generate()
+# mock.verify()
+UnitTest().test_spark_recon()
